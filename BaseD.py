35b5f8923b5d71af2c6c0316d18f1676e73cc3b3
Myr730 <A01735601@tec.mx>
2025-03-24 17:53:32 -0600

    Update BaseD.py

--- BaseD.py
+++ BaseD.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
     try:
         df_spark = spark.read.option("maxColumns", 20480).csv(file_path, header=True, inferSchema=True)
         df_spark.show(5)  # Verifica las primeras filas
-        df_spark.printSchema()  # Muestra la estructura de las columnas
+        df_spark.printSchema()  # Muestra la estructura de las columnass
     except Exception as e:
         print(f"Error al leer el CSV: {e}")
         spark.stop()
